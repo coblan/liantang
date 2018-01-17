0ddd1290ebd034897e6deb5097de8c09ee9a5399
heyulin <heyulin@smalltreemedia.com>
2018-01-17 11:45:47 +0800

    开启项目

---
+++ src/hello/engin_menu.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+
+from __future__ import unicode_literals
+
+from helpers.director.engine import BaseEngine,page,fa,page_dc
+
+class PcMenu(BaseEngine):
+    url_name='liantang'
+    menu=[
+        {'label':'指派巡查区域','url':page('geoinfo.dispatch'),'icon':fa('fa-map-o')},
+        {'label':'GIS区域','url':page('geoinfo.blockpolygon'),'icon':fa('fa-map-o')},
+        {'label':'区域组','url':page('geoinfo.blockgroup'),'icon':fa('fa-map-o')}
+    ]
+
+PcMenu.add_pages(page_dc)
