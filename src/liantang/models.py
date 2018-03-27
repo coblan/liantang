7bf1042621719b9b895d85a9a02688c8e94a0e82
coblan <coblan163.com>
2018-03-28 02:01:05 +0800

    ss

--- src/liantang/models.py
+++ src/liantang/models.py
@@ -45,7 +45,7 @@ class JianFangInfo(models.Model):
     addr = models.CharField('地址',max_length=500,blank=True)
     phone=models.CharField('电话号码',max_length=100,blank=True)
     state = models.IntegerField('当前流程',blank=True,choices=JIAN_STATE,default=-1)
-    shenqing = models.TextField(verbose_name='申请材料',default='{}')
+    shenqing = models.TextField(verbose_name='申请材料',blank=True,default='{}')
     xieyi = models.TextField(verbose_name='协议',blank=True,help_text='支持PDF和图片')
     xiugai=models.TextField(verbose_name='申请修改',blank=True,help_text='支持PDF和图片')
     other =models.TextField(verbose_name='其他',blank=True,help_text='支持PDF和图片')
