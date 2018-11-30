9faa7e3348eadfb3421cfa789a59ba032fdb2434
he_yulin <he_yulin@163.com>
2018-11-30 15:05:59 +0800

    debug

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -11,3 +11,9 @@ DATABASES = {
         'PORT': '5432', 
     },
 }
+
+#GDAL_LIBRARY_PATH = r'C:\Program Files\GDAL\gdal202'
+#GDAL_LIBRARY_PATH = r'C:\Program Files (x86)\GDAL\gdal202'
+#GDAL_LIBRARY_PATH =r'D:\tools\release-1900-x64-gdal-2-2-3-mapserver-7-0-7\bin\gdal202'
+#GDAL_DRIVER_PATH=r'D:\tools\release-1900-x64-gdal-2-2-3-mapserver-7-0-7\bin\gdal'
+#GDAL_LIBRARY_PATH = 'D:\tools\release-1900-x64-gdal-2-2-3-mapserver-7-0-7\bin\gdal'
